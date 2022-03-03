39cbd54a4edd52e4bbc7790c87f2d4905d6aae53
Riyaz-commits <skriyazbasha91@gmail.com>
2022-03-03 21:40:26 +0530

    adding manual browser

--- src/main/java/com/orangehrm/qa/base/Testbase.java
+++ src/main/java/com/orangehrm/qa/base/Testbase.java
@@ -67,9 +67,10 @@ public static void initialization()
 	 * driver=e_driver;
 	 */
 	if (System.getProperty("browser").equalsIgnoreCase("Chrome")) {
-		WebDriverManager.chromedriver().setup();
+		System.setProperty("webdriver.chrome.driver",".\\src\\main\\java\\com\\orangehrm\\qa\\util\\chromedriver.exe");
 		driver = new ChromeDriver();
-	} else if (System.getProperty("browser").equalsIgnoreCase("Firefox")) {
+	}
+		else if (System.getProperty("browser").equalsIgnoreCase("Firefox")) {
 		WebDriverManager.firefoxdriver().setup();
 		driver = new FirefoxDriver();
 	} else if (System.getProperty("browser").equalsIgnoreCase("IE")) {
